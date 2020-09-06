bb0f2ef87f6d6a49ec53339cebdbb5f2138bc0e3
Emanuel Pilz <emonadeo@gmail.com>
2020-09-07 00:59:02 +0200

    Initial server & client scaffold

---
+++ vilicus-server/src/main/java/de/marmeladenoma/vilicus/VilicusApplication.java
@@ -0,0 +1,12 @@
+package de.marmeladenoma.vilicus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VilicusApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(VilicusApplication.class);
+  }
+}
